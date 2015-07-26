5579869ae71cfed90e8c81bf3f1b93f7e105e5d1
Brian Gerson <brianpgerson@gmail.com>
2015-07-26 12:39:01 -0700

    wrote grow function

--- javascripts/src/plain-old-js-objects.js
+++ javascripts/src/plain-old-js-objects.js
@@ -1,8 +1,35 @@
-function createTree(){
-	console.log('shit');
-	
+var FRUIT_BEARING_AGE = 5;
+var MAX_AGE = 10;
+var orange = createOrange()
+
+function createTree(){	
 	return {
-		age: 0
+		age: 0,
+		height: 0,
+		orangeCount: 0,
+		isAlive: 1,
+		grow: grow,
+		dropOrange: dropOrange
 	}
 }
 
+function grow(){
+	this.age++;
+	this.height+=10;
+	if (this.age > MAX_AGE) {
+		this.isAlive = false;
+	} else if (this.age >= FRUIT_BEARING_AGE){
+		this.orangeCount += (Math.floor(Math.random() * 10));
+	}
+	console.log(this);
+}
+
+function dropOrange(){
+	return orange;
+}
+
+function createOrange(){
+	return {
+		diameter: (Math.ceil(Math.random() * 10)),
+	}
+}
