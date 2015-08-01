83565910588ab416c5b2c7508e070241e840ede7
Brian Gerson <brianpgerson@gmail.com>
2015-07-31 17:45:44 -0700

    moved the bindEventListener function to the controller

--- javascripts/src/view.js
+++ javascripts/src/view.js
@@ -3,27 +3,38 @@ window.addEventListener('load', init);
 
 function init(){
 	var view = new View;
-	view.bindEventListeners();
-	console.log(view);
+	var tree = new Tree;
+	var controller = new Controller(view, tree);
+	controller.bindEventListeners();
+	console.log(controller);
 }
 
 function View(){
 }
 
-View.prototype.bindEventListeners = function(){
-	document.getElementById("plant").addEventListener("click", this.nameTree.bind(this), false);	
-}
-
-View.prototype.nameTree = function(){
-    debugger;
-    var tree = prompt("Please enter your name", "Enter Here");
-    if (tree != null) {
-        document.getElementById("treeName").innerHTML = tree;
-    }
+View.prototype.nameTree = function(tree){
+    document.getElementById("treeName").innerHTML = tree;
     this.plantTree();
 }
 
 View.prototype.plantTree = function(){
 	document.getElementById("hide").setAttribute("id", "show");
 	document.getElementById("plant").setAttribute("id", "hide");
+}
+
+
+function Controller(view, tree){
+	this.view = view;
+	this.tree = tree;
+}
+
+Controller.prototype.nameTree = function(){
+    var tree = prompt("Please enter your name", "Enter Here");
+       if (tree != null) {
+       	this.view.nameTree(tree);
+    }
+}
+
+Controller.prototype.bindEventListeners = function(){
+	document.getElementById("plant").addEventListener("click", this.nameTree.bind(this), false);	
 }
