46fcd8ba56cc471054ec7c8d8b3d1a32c0a46023
Brian Gerson <brianpgerson@gmail.com>
2015-08-03 20:48:26 -0700

    wrote controller functions to check for oranges, then invoke view/model properties and prototypes to display those oranges on the tree at correct diameters/drop oranges when they are picked.

--- javascripts/src/view.js
+++ javascripts/src/view.js
@@ -22,12 +22,17 @@ View.prototype.plantTree = function(){
 	document.getElementById("plant").setAttribute("id", "hide");
 }
 
-View.prototype.addOrangeToTree = function(){
-	var randoMargin = Math.floor(Math.random() * (500 - 1) + 1);
-	document.getElementById("orange-overlay").innerHTML += "<div class='orange' style='margin: "+ randoMargin+ "px;'> </div>";
+View.prototype.addOrangeToTree = function(diameter){
+	var randoTop = Math.floor(Math.random() * (550 - 160) + 260);
+	var randoLeft = Math.floor(Math.random() * (700 - 50) + 150);
+	document.getElementById("orange-overlay").innerHTML += "<img src='images/orange.gif' height = '" + diameter + "px' class='orange' style='top: "+ randoTop+ "px; left: "+ randoLeft+ "px;'> </div>";
 }
 
 View.prototype.removeOrangeFromTree = function(){
+	var list = document.getElementById("orange-overlay");
+	list.removeChild(list.childNodes[0]);
+	list.removeChild(list.childNodes[1]);
+	console.log(list);
 
 }
 
